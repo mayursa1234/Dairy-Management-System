b39c3a28699f52f0b526f3c2a4701842f0d70746
Mayur Vijay Sawarbandhe <mayursawarbandhe4@gmail.com>
2026-06-18 22:17:32 +0530

    Add files via upload

---
+++ frontend/Register.jsx
@@ -0,0 +1,17 @@
+function Register(){
+    return(
+        <div>
+            <h1>Dairy Management System</h1>
+          
+          <form>
+            <input type="text" placeholder="Enter name" />
+            <input type="email" placeholder="Enter Email" />
+            <input type="password" placeholder="Enter Password"/>
+            <button type="submit">Register</button>
+          </form>
+
+        </div>
+    )
+}
+
+export default Register;
